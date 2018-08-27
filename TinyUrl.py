6e8b347cf6d5aa2355ae8e6a113b98cd4dccfa9d
txf0990 <txf0990@gmail.com>
2018-08-27 16:56:28 +0000

    Rename server.py as TinyUrl.py

---
+++ TinyUrl.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from flask import Flask, render_template, request, redirect, url_for, session
+import re
+import sqlite3
+import hashlib
+import base64
+from classes import Database
+
+filename = '/home/ubuntu/TinyUrl/TinyUrl.db'
+
+def md5(str):
+    m = hashlib.md5()   
+    m.update(str)
+    return m.hexdigest()
+
+def IfValidUrl(url):
+    if re.match(r'^https?:/{2}\w.+$', url):
+        return True
+    else:
+        return False
+
+app = Flask(__name__)
+
+def GenerateShort(LongAddr, digit):
+    str = md5(LongAddr)
+    str = base64.b64encode(str)
+    return str[:digit]
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    LongAddr = ""
+    ShortAddr = ""
+    status = 0
+    # status=0: Nothing input.
+    # status=1: Input is not a valid url.
+    # status=2: You are trying to visit a short address not existing in the database.
+    if (request.method == 'POST'):
+        LongAddr = request.form['LongAddr']
+        if (not IfValidUrl(LongAddr)):
+            status = 1
+            return render_template('home.html', ShortAddr=ShortAddr, LongAddr=LongAddr, status=status)
+        database = Database(filename)
+        digit = 4;
+        ShortAddr = GenerateShort(LongAddr,digit)
+        search = database.find(ShortAddr)
+        while(search != ''):
+            if (search == LongAddr):
+                break;
+            else:
+                digit = digit + 1
+                ShortAddr = GenerateShort(LongAddr, digit)
+                search = database.find(ShortAddr)
+        if (search == ''):
+            database.insert(ShortAddr, LongAddr)
+        ShortAddr = 'http://hoogle.xyz/'+ShortAddr
+    return render_template('home.html', ShortAddr=ShortAddr, LongAddr=LongAddr, status=status)
+
+@app.route('/<ShortAddr>')
+def visit(ShortAddr):
+    database = Database(filename)
+    LongAddr = database.find(ShortAddr)
+    if (LongAddr == ''):
+        return render_template('home.html', ShortAddr="", LongAddr="", status=2)
+    else:
+        return redirect(LongAddr)
+
+if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=80)
+    app.run()
